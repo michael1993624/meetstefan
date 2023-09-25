da0b2b4f39457cf948e7f9922d6d41c0410b634f
unknown <michael1993624@gmail.com>
2023-09-26 00:54:34 +0900

    changed document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -14,7 +14,7 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap"
           rel="stylesheet"
-        />
+        /> */}
       </Head>
       <title>SocialAGI Dev</title>
       <body>
