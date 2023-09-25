12b00ebf47286f4ce4350e9fde5e0e53413c243d
unknown <michael1993624@gmail.com>
2023-09-26 01:21:31 +0900

    aa

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { Html, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <title>SocialAGI Dev</title>
       <body>
-        <Main></Main>
+        <Main />
         <NextScript />
       </body>
     </Html>
